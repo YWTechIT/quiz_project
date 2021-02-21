4ccbb3fd85981376084d56260dc35c42522bb760
YWTechIT <abcd8637@naver.com>
2021-02-21 20:33:50 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Buttons from "./modules/Buttons";
+import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="app">
+          <div className="question-section">
+            <h1 className="question-header">
+              <span>1</span>/4
+            </h1>
+            <div className="question-text">
+              일론 머스크의 우주 탐사 기업 이름은?
+            </div>
+          </div>
+          <div className="answer-section">
+           <Buttons /> 
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
